78bb10e9f4b888f91fde5487d13260d45a11e859
Aaisuiu <65217163+Aaisuiu@users.noreply.github.com>
2021-09-13 10:55:32 +0800

    Update index.js

--- yuketang/index.js
+++ yuketang/index.js
@@ -78,23 +78,54 @@ var sgin = async () => {
             sessionid: setCookies[1].slice(10, 42)
         }
         const cookie = `csrftoken=${cookies['csrftoken']};sessionid=${cookies['sessionid']};django_language=zh-cn;`
-        const on_lesson_courses = await Axios({
+        // 🐟课堂有两个接口
+        // console.log(1);
+        const on_lesson_courses1 = await Axios({
             method: 'get',
             url: config.baseurl + '/v/course_meta/on_lesson_courses',
             headers: { 'Cookie': cookie },
             withCredentials: true
-        })
-        console.log(on_lesson_courses.data);
-        if (on_lesson_courses.data['data']['on_lessons'] == '') throw (new Date() + `  尚未有课程`);
-        const lessonName = on_lesson_courses.data['data']['on_lessons'][0]['name'];
-        const lessonId = on_lesson_courses.data['data']['on_lessons'][0]['lesson_id']
-        const signIn = await Axios({
-            method: 'get',
-            url: config.baseurl + `/v/lesson/lesson_info_v2?lesson_id=${lessonId}&source=5`,
-            headers: { 'Cookie': cookie },
-        })
-        if (signIn.data != undefined) console.log(lessonName + "签到成功" + new Date());
+        });
 
+        const on_lesson_courses3 = await Axios({
+            method: 'get',
+            url: config.baseurl + '/api/v3/classroom/on-lesson',
+            headers: { 'Cookie': cookie },
+            withCredentials: true
+        });
+        // console.log(on_lesson_courses3.data['data']['onLessonClassrooms'][0]);
+        const v1Lesson = on_lesson_courses1.data['data'] ? on_lesson_courses1.data['data']['on_lessons'][0] : [];
+        const v3Lesson = on_lesson_courses3.data['data'] ? on_lesson_courses3.data['data']['onLessonClassrooms'][0] : [];
+        if (!v1Lesson && !v3Lesson) throw (new Date() + `  尚未有课程`);
+        if (v1Lesson) {
+
+            const lessonName = v1Lesson['name'];
+            const lessonId = v1Lesson['lesson_id'];
+            const signIn = await Axios({
+                method: 'get',
+                url: config.baseurl + `/v/lesson/lesson_info_v2?lesson_id=${lessonId}&source=5`,
+                headers: { 'Cookie': cookie },
+            })
+            if (signIn.data != undefined) console.log(lessonName + " 签到成功 " + new Date());
+
+        }
+        if (v3Lesson) {
+            const lessonName = v3Lesson['courseName'];
+            const lessonId = v3Lesson['lessonId'];
+            const signIn = await Axios({
+                method: 'post',
+                url: config.baseurl + `/api/v3/lesson/checkin`,
+                headers: {
+                    'Cookie': cookie,
+                    'Referer': 'https://changjiang.yuketang.cn/lesson'
+                },
+                data: {
+                    'source': 5,
+                    'lessonId': lessonId
+                }
+            })
+            if (signIn.data['code'] == 0) console.log(lessonName + " 签到成功 " + new Date());
+        }
 
     }
     catch (e) {
